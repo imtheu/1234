07a2a7bab7d6bf32b1df47d5c101656d4f9dc78d
ïmtheu <matheusfrmorais@hotmail.com>
2024-01-14 21:19:58 -0300

    feat(index.js): add onPlay fn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+let step;
+let start;
+let beatMax;
+let beatCount = 0;
+let time = 0;
+
+const audios = [
+  new Audio("./sounds/metronome.mp3"),
+  new Audio("./sounds/metronome.mp3"),
+];
+
 const hasVideoElement = async () => {
   const [{ id: tabId }] = await chrome.tabs.query({ active: true });
 
@@ -14,6 +25,47 @@ const hasVideoElement = async () => {
   );
 };
 
+const playVideo = async () => {
+  const [{ id: tabId }] = await chrome.tabs.query({ active: true });
+
+  chrome.scripting.executeScript({
+    target: {
+      tabId,
+    },
+    func: () => {
+      document.querySelector("video")?.play();
+    },
+  });
+};
+
+const beat = () => {
+  if (beatCount === beatMax) {
+    playVideo();
+  } else {
+    audios[beatCount % 2].play();
+  }
+
+  time += step;
+  const diff = new Date().getTime() - start - time;
+
+  if (beatCount < beatMax) {
+    window.setTimeout(beat, step - diff);
+    beatCount++;
+  }
+};
+
+const onPlay = () => {
+  time = 0;
+  beatCount = 0;
+  beatMax = 4;
+
+  const bpm = 128;
+  start = new Date().getTime();
+  step = 60000 / bpm;
+
+  window.setTimeout(beat, step);
+};
+
 (async () => {
   const noVideoContentElement = document.getElementById("noVideo");
   const hasVideoContentElement = document.getElementById("hasVideo");
@@ -22,4 +74,7 @@ const hasVideoElement = async () => {
 
   hasVideoContentElement.classList.toggle("isVisible", hasVideo);
   noVideoContentElement.classList.toggle("isVisible", !hasVideo);
+
+  const playButton = document.getElementById("playButton");
+  playButton.addEventListener("click", onPlay);
 })();
