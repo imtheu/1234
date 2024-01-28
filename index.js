d80913387f471b8b70a2f9a3aabd9e378acac1be
ïmtheu <matheusfrmorais@hotmail.com>
2024-01-28 20:20:33 -0300

    feat(video): get video state

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 import { startCounter } from "./src/counter.js";
-import { hasVideoElement, playVideo } from "./src/video.js";
+import {
+  addVideoListeners,
+  getInitialVideoState,
+  playVideo,
+} from "./src/video.js";
 
 (async () => {
   const noVideoContentElement = document.getElementById("noVideo");
   const hasVideoContentElement = document.getElementById("hasVideo");
+  const mainButtonContainerElement = document.getElementById("mainButton");
 
-  const hasVideo = await hasVideoElement();
+  chrome.runtime.onMessage.addListener((req) => {
+    if (req.startsWith("VIDEO:")) {
+      const action = req.split(":")[1];
+      const isPlaying = action === "PLAY";
+      mainButtonContainerElement.classList.toggle("isPlaying", isPlaying);
+    }
+  });
 
-  hasVideoContentElement.classList.toggle("isVisible", hasVideo);
-  noVideoContentElement.classList.toggle("isVisible", !hasVideo);
+  try {
+    const { playing } = await getInitialVideoState();
+    mainButtonContainerElement.classList.toggle("isPlaying", playing);
+
+    hasVideoContentElement.classList.toggle("isVisible", true);
+    noVideoContentElement.classList.toggle("isVisible", false);
+
+    await addVideoListeners();
+  } catch {
+    hasVideoContentElement.classList.toggle("isVisible", false);
+    noVideoContentElement.classList.toggle("isVisible", true);
+  }
 
   const playButton = document.getElementById("playButton");
   playButton.addEventListener("click", () => startCounter().then(playVideo));
--- src/video.js
+++ src/video.js
@@ -1,15 +1,32 @@
-export const hasVideoElement = async () => {
+export const getInitialVideoState = async () => {
   const [{ id: tabId }] = await chrome.tabs.query({ active: true });
 
-  return new Promise((resolve) =>
+  return new Promise((resolve, reject) =>
     chrome.scripting.executeScript(
       {
         target: {
           tabId,
         },
-        func: () => !!document.querySelector("video"),
+        func: () => {
+          const video = document.querySelector("video");
+          if (video) {
+            return {
+              playing: !video.paused && !video.ended && video.readyState > 2,
+            };
+          }
+
+          return {
+            error: "NOT_FOUND",
+          };
+        },
       },
-      ([{ result }]) => resolve(result)
+      ([{ result }]) => {
+        if (result.error) {
+          return reject(result.error);
+        }
+
+        resolve(result);
+      }
     )
   );
 };
@@ -22,5 +39,35 @@ export const playVideo = async () => {
       tabId,
     },
     func: () => document.querySelector("video")?.play(),
+  });
+};
+
+export const addVideoListeners = async () => {
+  const [{ id: tabId }] = await chrome.tabs.query({ active: true });
+
+  chrome.scripting.executeScript({
+    target: {
+      tabId,
+    },
+    func: () => {
+      const video = document.querySelector("video");
+      const sendMessage = chrome.runtime.sendMessage;
+
+      if (window._1234) {
+        video.removeEventListener("play", window._1234.onPlay);
+        video.removeEventListener("pause", window._1234.onPause);
+        video.removeEventListener("ended", window._1234.onEnded);
+      } else {
+        window._1234 = {};
+      }
+
+      window._1234.onPlay = () => sendMessage("VIDEO:PLAY");
+      window._1234.onPause = () => sendMessage("VIDEO:PAUSE");
+      window._1234.onEnded = () => sendMessage("VIDEO:ENDED");
+
+      video.addEventListener("play", window._1234.onPlay);
+      video.addEventListener("pause", window._1234.onPause);
+      video.addEventListener("ended", window._1234.onEnded);
+    },
   });
 };
